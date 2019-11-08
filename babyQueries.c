f0976a150354d3913cc4ea32d2474044eaa9bac7
AaronBester <bestera@uoguelph.ca>
2019-11-08 00:01:09 -0500

    Fixed up babyQueries

--- babyQueries.c
+++ babyQueries.c
@@ -5,15 +5,7 @@
 #include "babies.h"
 
 int main(){
-/*
-  struct pNames {
-     int  year;
-     int  rank[ROWS];
-     char maleName[ROWS][MAXLENGTH];
-     int  maleNumber[ROWS];
-     char femaleName[ROWS][MAXLENGTH];
-     int  femaleNumber[ROWS];
-  };
+
 char userResponse[200];
 char maleSNumber[100];
 char femaleSNumber[100];
@@ -22,8 +14,10 @@ char fileName1[14];
 char fileName2[14];
 char searchName[200];
 
-int maleNameFound = 0;
-int femaleNameFound = 0;
+int maleNameDecade1Found = 0;
+int maleNameDecade2Found = 0;
+int femaleNameDecade1Found = 0;
+int femaleNameDecade2Found = 0;
 int stopLoop = 0;
 int decadeLoop = 0;
 int decade1 = 0;
@@ -37,6 +31,9 @@ int maleFemSearchLoop = 0;
 int rankNum = 0;
 int decadeReturnLoop = 0;
 int finalQuestionLoop = 0;
+int decadeEqualCheck = 0;
+int decade1Loop = 0;
+int decade2Loop = 0;
 
 FILE *f1;
 FILE *f2;
@@ -45,146 +42,158 @@ FILE *f2;
 
 while(stopLoop == 0){
 
-  while(decadeLoop < 2){
+    while(decade2Loop == 0){
+while(decade1Loop == 0){
 
-    printf("What decade do you want to look at? [1880 to 2010]\n");
-    fgets(userResponse, 200, stdin);
-    sscanf(userResponse, %d, %d, decade1,decade2 )
+        while(decadeEqualCheck == 0){
+          printf("What decade do you want to look at? [1880 to 2010]\n");
+          fgets(userResponse, 100, stdin);
+          sscanf(userResponse, "%d %d", &decade1, &decade2);
 
-
-
-    if(decade1 == 1880){
+          if(decade1 == decade2){
+            printf("Acceptable decades are 1880, 1890, 1900, 1910, 1920, 1930, 1940, 1950, 1960,1970, 1980, 1990, 2000, or 2010. No other numbers are acceptable. You must enter 2 acceptable decades separated by a least one space.\n");
+          }
+          else{
+            decadeEqualCheck = 1;
+          }
+        }
+        decadeEqualCheck = 0;
+   if(decade1 == 1880){
       strcpy(fileName1,"1880Names.txt");
-      decadeLoop++;
+      decade1Loop = 1;
     }
 
     else if(decade1 == 1890){
       strcpy(fileName1,"1890Names.txt");
-        decadeLoop++;
+      decade1Loop = 1;
     }
 
     else if(decade1 == 1900){
       strcpy(fileName1,"1900Names.txt");
-        decadeLoop++;
+      decade1Loop = 1;
     }
 
     else if(decade1 == 1910){
       strcpy(fileName1,"1910Names.txt");
-        decadeLoop++;
+      decade1Loop = 1;
     }
     else if(decade1 == 1920){
       strcpy(fileName1,"1920Names.txt");
-        decadeLoop++;
+      decade1Loop = 1;
     }
     else if(decade1 == 1930){
       strcpy(fileName1,"1930Names.txt");
-        decadeLoop++;
+      decade1Loop = 1;
     }
     else if(decade1 == 1940){
       strcpy(fileName1,"1940Names.txt");
-        decadeLoop++;
+      decade1Loop = 1;
     }
     else if(decade1 == 1950){
       strcpy(fileName1,"1950Names.txt");
-        decadeLoop++;
+      decade1Loop = 1;
     }
     else if(decade1 == 1960){
       strcpy(fileName1,"1960Names.txt");
-        decadeLoop++;
+      decade1Loop = 1;
     }
     else if(decade1 == 1970){
       strcpy(fileName1,"1970Names.txt");
-        decadeLoop++;
+      decade1Loop = 1;
     }
     else if(decade1 == 1980){
       strcpy(fileName1,"1980Names.txt");
-        decadeLoop++;
+      decade1Loop = 1;
     }
     else if(decade1 == 1990){
       strcpy(fileName1,"1990Names.txt");
-        decadeLoop++;
+      decade1Loop = 1;
     }
     else if(decade1 == 2000){
       strcpy(fileName1,"2000Names.txt");
-        decadeLoop++;
+      decade1Loop = 1;
     }
     else if(decade1 == 2010){
       strcpy(fileName1,"2010Names.txt");
-        decadeLoop++;
+      decade1Loop = 1;
     }
 
     else{
-      printf("Error - Decade entered does not exist. Please enter decade between 1880 and 2010\n");
+      printf("Acceptable decades are 1880, 1890, 1900, 1910, 1920, 1930, 1940, 1950, 1960,1970, 1980, 1990, 2000, or 2010. No other numbers are acceptable. You must enter 2 acceptable decades separated by a least one space.\n");
     }
+    }
+
+    decade1Loop = 0;
 
     if(decade2 == 1880){
       strcpy(fileName2,"1880Names.txt");
-        decadeLoop++;
+          decade2Loop = 1;
     }
 
     else if(decade2 == 1890){
       strcpy(fileName2,"1890Names.txt");
-        decadeLoop++;
+          decade2Loop = 1;
     }
 
     else if(decade2 == 1900){
       strcpy(fileName2,"1900Names.txt");
-        decadeLoop++;
+          decade2Loop = 1;
     }
 
     else if(decade2 == 1910){
       strcpy(fileName2,"1910Names.txt");
-        decadeLoop++;
+        decade2Loop = 1;
 
     }
 
     else if(decade2 == 1920){
       strcpy(fileName2,"1920Names.txt");
-        decadeLoop++;
+          decade2Loop = 1;
     }
     else if(decade2 == 1930){
       strcpy(fileName2,"1930Names.txt");
-        decadeLoop++;
+          decade2Loop = 1;
     }
     else if(decade2 == 1940){
       strcpy(fileName2,"1940Names.txt");
-        decadeLoop++;
+          decade2Loop = 1;
     }
     else if(decade2 == 1950){
       strcpy(fileName2,"1950Names.txt");
-        decadeLoop++;
+          decade2Loop = 1;
     }
     else if(decade2 == 1960){
       strcpy(fileName2,"1960Names.txt");
-        decadeLoop++;
+        decade2Loop = 1;
     }
     else if(decade2 == 1970){
       strcpy(fileName2,"1970Names.txt");
-        decadeLoop++;
+          decade2Loop = 1;
     }
     else if(decade2 == 1980){
       strcpy(fileName2,"1980Names.txt");
-        decadeLoop++;
+        decade2Loop = 1;
     }
     else if(decade2 == 1990){
       strcpy(fileName2,"1990Names.txt");
-      decadeLoop++;
+        decade2Loop = 1;
     }
     else if(decade2 == 2000){
       strcpy(fileName2,"2000Names.txt");
-        decadeLoop++;
+          decade2Loop = 1;
     }
     else if(decade2 == 2010){
       strcpy(fileName2,"2010Names.txt");
-        decadeLoop++;
+          decade2Loop = 1;
     }
 
     else{
-      printf("Error - Decade entered does not exist. Please enter decade between 1880 and 2010\n");
+      printf("Acceptable decades are 1880, 1890, 1900, 1910, 1920, 1930, 1940, 1950, 1960,1970, 1980, 1990, 2000, or 2010. No other numbers are acceptable. You must enter 2 acceptable decades separated by a least one space.\n");
     }
-  }
 
-  }
+}
+
+
 
     popular.year = decade1;
     if ((f1 = fopen(fileName1, "r")) !=NULL){
@@ -250,17 +259,27 @@ while(stopLoop == 0){
               fgets(userResponse, 200, stdin);
 
               if((atoi(userResponse)) == 0){
-                printf("Rank %d: %d Male:%s (%d) \n %d: Male: %s %d",rankNum,decade1, popular.maleName[rankNum-1], popular.maleNumber[rankNum-1],decade2,popularYear2.maleName[rankNum-1],popularYear2.maleNumber[rankNum-1]);
+                printf("Rank %d: %d Male:%s (%d) \n%d: Male: %s %d\n",rankNum,decade1, popular.maleName[rankNum-1], popular.maleNumber[rankNum-1],decade2, popularYear2.maleName[rankNum-1],popularYear2.maleNumber[rankNum-1]);
                 maleFemRankLoop = 1;
               }
 
               else if((atoi(userResponse)) == 1){
-                printf("Rank %d: %d Female:%s (%d) \n %d: Female: %s %d",rankNum, decade1, popular.femaleName[rankNum-1], popular.femaleNumber[rankNum-1]), decade2,popularYear2.femaleName[rankNum-1],femaleNumber.popularYear2[rankNum-1];
+              printf("Rank %d: %d Female:%s (%d) \n"
+                     "     %d: Female: %s %d\n",
+                     rankNum,decade1, popular.femaleName[rankNum-1],
+                     popular.femaleNumber[rankNum-1],decade2,
+                     popularYear2.femaleName[rankNum-1],popularYear2.femaleNumber[rankNum-1]);
                 maleFemRankLoop = 1;
               }
 
               else if((atoi(userResponse)) == 2){
-                printf("Rank %d: Male:%s (%d) and Female:%s (%d) \n",rankNum, popular.maleName[rankNum-1], popular.maleNumber[rankNum-1], popular.femaleName[rankNum-1],popular.femaleNumber[rankNum-1]);
+                printf("Rank %d: %d: Male:%s (%d) and Female:%s (%d) \n"
+                "Rank %d: %d: Male:%s (%d) and Female:%s (%d) \n",
+                rankNum, decade1, popular.maleName[rankNum-1],
+                popular.maleNumber[rankNum-1], popular.femaleName[rankNum-1],
+                popular.femaleNumber[rankNum-1],rankNum,decade2,popularYear2.maleName[rankNum-1],
+                popularYear2.maleNumber[rankNum-1],popularYear2.femaleName[rankNum-1],
+                popularYear2.femaleNumber[rankNum-1]);
                 maleFemRankLoop = 1;
               }
               else{
@@ -291,30 +310,48 @@ while(stopLoop == 0){
               maleFemSearchLoop = 1;
               for(i = 0; i < 200; i++){
                 if((strcmp(searchName,popular.maleName[i])) == 0){
-                  maleNameFound = i;
+                  maleNameDecade1Found = i;
+                }
+                if((strcmp(searchName,popularYear2.maleName[i])) == 0){
+                  maleNameDecade2Found = i;
                 }
               }
-              if(maleNameFound != 0){
-                  printf("In %d the male name %s is ranked %d with a count of %d.\n", decade, searchName, popular.rank[maleNameFound], popular.maleNumber[maleNameFound] );
+              if(maleNameDecade1Found != 0){
+                  printf("In %d the male name %s is ranked %d with a count of %d.\n", decade1, searchName, popular.rank[maleNameFound], popular.maleNumber[maleNameFound] );
               }
               else{
-                printf("In %d, the male name %s is not ranked.\n",decade, searchName );
+                printf("In %d, the male name %s is not ranked.\n",decade1, searchName );
               }
+              if(maleNameDecade2Found!= 0){
+                  printf("In %d the male name %s is ranked %d with a count of %d.\n", decade2, searchName, popularYear2.rank[maleNameFound], popularYear2.maleNumber[maleNameFound] );
+              }
+              else{
+                  printf("In %d, the male name %s is not ranked.\n",decade2, searchName );
+              }
+
             }
 
             else if((atoi(userResponse)) == 1){
                   maleFemSearchLoop = 1;
               for(i = 0; i < 200; i++){
                 if((strcmp(searchName,popular.femaleName[i])) == 0){
-                  femaleNameFound = i;
+                  femaleNameDecade1Found = i;
                 }
-
+                if((strcmp(searchName,popularYear2.femaleName[i])) == 0){
+                  femaleNameDecade2Found = i;
+                }
               }
-              if(femaleNameFound != i){
-                  printf("In %d the female name %s is ranked %d with a count of %d.\n", decade, searchName, popular.rank[femaleNameFound], popular.femaleNumber[femaleNameFound] );
+              if(femaleNameDecade1Found != i){
+                  printf("In %d the female name %s is ranked %d with a count of %d.\n", decade1, searchName, popular.rank[femaleNameFound], popular.femaleNumber[femaleNameFound] );
               }
               else{
-                printf("In %d, the male name %s is not ranked.\n",decade, searchName );
+                printf("In %d, the male name %s is not ranked.\n",decade1, searchName );
+              }
+              if(femaleNameDecade2Found != i){
+                  printf("In %d the female name %s is ranked %d with a count of %d.\n", decade2, searchName, popularYear2.rank[femaleNameFound], popularYear2.femaleNumber[femaleNameFound] );
+              }
+              else{
+                printf("In %d, the male name %s is not ranked.\n",decade2, searchName );
               }
             }
 
@@ -323,26 +360,47 @@ while(stopLoop == 0){
               for(i = 0; i < 200; i++){
 
                 if((strcmp(searchName,popular.maleName[i])) == 0){
-                  maleNameFound = i;
+                  maleNameDecade1Found = i;
                 }
 
 
                 if((strcmp(searchName,popular.femaleName[i])) == 0){
-                  femaleNameFound = i;
+                  femaleNameDecade1Found = i;
+                }
+
+                if((strcmp(searchName,popularYear2.maleName[i])) == 0){
+                  maleNameDecade2Found = i;
+                }
+
+
+                if((strcmp(searchName,popularYear2.femaleName[i])) == 0){
+                  femaleNameDecade2Found = i;
                 }
 
               }
-              if((maleNameFound != 0) && (femaleNameFound != 0)){
-                printf("In %d the female name %s is ranked %d with a count of %d and the male name %s is ranked %d with a count of %d.\n", decade, searchName, popular.rank[femaleNameFound], popular.femaleNumber[femaleNameFound], searchName, popular.rank[maleNameFound],popular.maleNumber[maleNameFound] );
+              if((maleNameDecade1Found != 0) && (femaleNameDecade1Found != 0)){
+                printf("In %d the female name %s is ranked %d with a count of %d and the male name %s is ranked %d with a count of %d.\n", decade1, searchName, popular.rank[femaleNameFound], popular.femaleNumber[femaleNameFound], searchName, popular.rank[maleNameFound],popular.maleNumber[maleNameFound] );
               }
-              else if((maleNameFound != 0) && (femaleNameFound == 0)){
-                printf("In %d the female name %s is not ranked and the male name %s is ranked %d with a count of %d\n", decade, searchName, searchName, popular.rank[maleNameFound], popular.maleNumber[maleNameFound] );
+              else if((maleNameDecade1Found != 0) && (femaleNameDecade1Found == 0)){
+                printf("In %d the female name %s is not ranked and the male name %s is ranked %d with a count of %d\n", decade1, searchName, searchName, popular.rank[maleNameFound], popular.maleNumber[maleNameFound] );
               }
-              else if((maleNameFound == 0) && (femaleNameFound != 0)){
-                printf("In %d the female name %s is ranked %d with a count of %d and the male name %s is not ranked\n", decade, searchName, popular.rank[femaleNameFound],popular.femaleNumber[femaleNameFound], searchName );
+              else if((maleNameDecade1Found == 0) && (femaleNameDecade1Found != 0)){
+                printf("In %d the female name %s is ranked %d with a count of %d and the male name %s is not ranked\n", decade1, searchName, popular.rank[femaleNameFound],popular.femaleNumber[femaleNameFound], searchName );
               }
               else{
-                printf("In %d the female name %s is not ranked and the male name %s is not ranked\n", decade,searchName,searchName );
+                printf("In %d the female name %s is not ranked and the male name %s is not ranked\n", decade1,searchName,searchName );
+              }
+              if((maleNameDecade2Found != 0) && (femaleNameDecade2Found != 0)){
+                printf("In %d the female name %s is ranked %d with a count of %d and the male name %s is ranked %d with a count of %d.\n", decade2, searchName, popularYear2.rank[femaleNameFound], popularYear2.femaleNumber[femaleNameFound], searchName, popularYear2.rank[maleNameFound],popularYear2.maleNumber[maleNameFound] );
+              }
+              else if((maleNameDecade2Found != 0) && (femaleNameDecade2Found == 0)){
+                printf("In %d the female name %s is not ranked and the male name %s is ranked %d with a count of %d\n", decade2, searchName, searchName, popularYear2.rank[maleNameFound], popularYear2.maleNumber[maleNameFound] );
+              }
+              else if((maleNameDecade2Found == 0) && (femaleNameDecade2Found != 0)){
+                printf("In %d the female name %s is ranked %d with a count of %d and the male name %s is not ranked\n", decade2, searchName, popularYear2.rank[femaleNameFound],popularYear2.femaleNumber[femaleNameFound], searchName );
+              }
+              else{
+                printf("In %d the female name %s is not ranked and the male name %s is not ranked\n", decade2,searchName,searchName );
               }
             }
             else{
@@ -363,7 +421,7 @@ while(stopLoop == 0){
       }
     }
   while (decadeReturnLoop == 0) {
-    printf("Do you want to ask another question about %d? [Y or N]\n", decade);
+    printf("Do you want to ask another question about %d? [Y or N]\n", decade1);
     fgets(userResponse, 200, stdin);
     if(userResponse[0] == *("Y")){
       decadeReturnLoop = 1;
@@ -403,7 +461,6 @@ while(stopLoop == 0){
   decadeReturnLoop = 0;
   finalQuestionLoop = 0;
   }
-*/
+
 return(0);
 }
-
